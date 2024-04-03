61903cb05a56f6ac71ec3eccd9497bcd4ef9f15f
iveckin <iveckin123@gmail.com>
2024-04-03 15:36:26 +0200

    lol

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,9 @@ function Header() {
   return (
     <div className="header-wrapper">
       <nav className="nav">
+
         <ul>
+          <li><a class="logo"><span>kukni</span>.si</a></li>
           <li><a href="/">Home</a></li>
           <li><a href="/about">About</a></li>
           <li><a href="/contact">Contact</a></li>
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,14 +6,20 @@ import '../css/MovieList.css'; // Import the CSS file
 function MovieList() {
   return (
     <div className='black'>
+      
       <ul className="movie-list">
+        
         {movieData.map(movie => (
-          <li key={movie.id} className="movie-list-item">
-            <Link to={`/movies/${movie.id}`}>
-              <img src={movie.poster} alt={movie.title} className="movie-image" />
-            </Link>
-            <h3 className="movie-title">{movie.title}</h3>
-            {/* Display other movie information as needed */}
+          <li key={movie.id} className={`movie-list-item ${movie.colorRating == "good" ? 'glow-red' : 'glow-blue'}`}>
+            <div className="movie-image-container">
+            <span className={`movie-rating ${movie.colorRating == "good" ? 'rating-red' : 'rating-blue'}`}>{movie.rating}</span>
+              
+              <Link to={`/movies/${movie.id}`}>
+                <img src={movie.poster} alt={movie.title} className="movie-image" />
+              </Link>
+              
+              <h3 className="movie-title">{movie.title}</h3>
+            </div>
           </li>
         ))}
       </ul>
